6792dce534a8cdef2a62c63d4190c316f00ff813
arce <2326@gmail.com>
2025-01-21 14:21:51 -0500

    new url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-export const API_URL = window.location.origin === "localhost" ?
-"https://adamarce-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "https://adamarce-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
+export const API_URL = window.location.hostname === "localhost" ? "https://adamarce-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "https://adamarce-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
 console.log(
     "API_URL :",
     API_URL
